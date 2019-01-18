bbdae628e221598a3875e531b2589438227be4dc
AdriaanPeetermans <adriaanpeetermans@live.be>
2019-01-18 23:27:08 +0100

    Added drawing server.

--- WeatherStationSite/WebContent/Scripts/nl/dixit/playersDrawing.js
+++ WeatherStationSite/WebContent/Scripts/nl/dixit/playersDrawing.js
@@ -37,6 +37,7 @@ var playerColors = [];
 var numberPlayers;
 var playersReady = [false, false, false, false, false, false, false, false];
 var startingTime;
+var playerNames = [];
 
 function getParams() {
 	var queryString = decodeURIComponent(window.location.search);
@@ -45,12 +46,11 @@ function getParams() {
 	serverPort = parseInt(queries[0].split("=")[1]);
 	startTime = parseInt(queries[1].split("=")[1]);
 	nowTime = startTime;
-	//window.setInterval(timer, 1000);
 	
 	connection = new WebSocket(serverAddress + serverPort);
 	
 	connection.onopen = function() {
-		connection.send("getColorsHost");
+		connection.send("getPlayerNames");
 	};
 	
 	connection.onerror = function (error) {
@@ -78,6 +78,14 @@ function getParams() {
 				startingTime = d.getTime();
 				window.setInterval(timer, 1000);
 				window.setInterval(linesTimer, lineDelay);
+				drawInit();
+				break;
+			case "playerNames":
+				var number = parseInt(parts[1]);
+				for (var i = 0; i < number; i++) {
+					playerNames[i] = parts[i+2];
+				}
+				connection.send("getColorsHost");
 				break;
 		}
 	};
@@ -99,12 +107,14 @@ function drawPlayerLine(playerIndex, startP, endP) {
 	if (playerIndex % 2 == 0) {
 		startX = width*(1-startP);
 		endX = width*(1-endP);
-		mainCtx.clearRect(endX-vSpacing*0.6, startY-vSpacing*0.45, startX-endX+vSpacing*0.6, vSpacing*1.45);
+		mainCtx.clearRect(endX-vSpacing*0.6, startY-vSpacing*0.45, startX-endX+vSpacing*0.6, vSpacing*1.1);
+		mainCtx.textAlign = "left";
 	}
 	else {
 		startX = width*startP;
 		endX = width*endP;
-		mainCtx.clearRect(startX, startY-vSpacing*0.45, endX+vSpacing*0.6-startX, vSpacing*1.45);
+		mainCtx.clearRect(startX, startY-vSpacing*0.45, endX+vSpacing*0.6-startX, vSpacing*1.1);
+		mainCtx.textAlign = "right";
 	}
 	mainCtx.fillStyle = playerColors[playerIndex].color1;
 	mainCtx.beginPath();
@@ -123,21 +133,32 @@ function drawPlayerLine(playerIndex, startP, endP) {
 	mainCtx.shadowOffsetX = 0;
 	mainCtx.shadowOffsetY = 6;
 	mainCtx.stroke();
+	
+	mainCtx.shadowColor = 'rgba(0, 0, 0, 0)';
+	mainCtx.fillStyle = "#2C3E50";
+	mainCtx.textBaseline = "middle"
+	mainCtx.font = vSpacing*0.5 + "px Arial";
+	mainCtx.fillText(playerNames[playerIndex], endX, startY);
 }
 
 function finishLine(playerIndex, startP, endP) {
 	drawPlayerLine(playerIndex, startP, endP);
-	var pieceWidth = 0.1;
+	//var pieceWidth = 0.1;
 	var vSpacing = (height - titleHeight)/numberPlayers;
 	var endX;
 	var startY = titleHeight + vSpacing*(playerIndex+0.5);
+	var pieceWidth = mainCtx.measureText(playerNames[playerIndex]).width;
 	if (playerIndex % 2 == 0) {
 		endX = width*(1-endP);
-		startX = endX + width*pieceWidth;
+		//startX = endX + width*pieceWidth;
+		startX = endX + pieceWidth;
+		mainCtx.textAlign = "left";
 	}
 	else {
 		endX = width*endP;
-		startX = endX - width*pieceWidth;
+		//startX = endX - width*pieceWidth;
+		startX = endX - pieceWidth;
+		mainCtx.textAlign = "right";
 	}
 	mainCtx.fillStyle = playerColors[playerIndex].color2;
 	mainCtx.beginPath();
@@ -151,33 +172,29 @@ function finishLine(playerIndex, startP, endP) {
 	mainCtx.shadowOffsetX = 0;
 	mainCtx.shadowOffsetY = 0;
 	mainCtx.stroke();
+	
+	mainCtx.shadowColor = 'rgba(0, 0, 0, 0)';
+	mainCtx.fillStyle = "#E0E0E0";
+	mainCtx.textBaseline = "middle"
+	mainCtx.font = vSpacing*0.5 + "px Arial";
+	mainCtx.fillText(playerNames[playerIndex], endX, startY);
 }
 
 function timer() {
+	var oldSecond = nowTime%60;
+	var oldMinute = (nowTime-oldSecond)/60;
 	nowTime --;
-	if (nowTime == 148) {
-		playersReady[3] = true;
-		var d = new Date();
-		var t = d.getTime();
-		var perc = (t-startingTime)/1000.0/startTime;
-		perc = 0.1 + perc*0.8;
-		finishLine(3, 0, perc);
+	var second = nowTime%60;
+	var minute = (nowTime-second)/60;
+	shifterIntervals[4] = window.setInterval(function() { shiftChar(stringLength(oldSecond, 2).substring(1,2), stringLength(second, 2).substring(1,2), 4); }, shiftDelay);
+	if (stringLength(oldSecond, 2).substring(0,1) != stringLength(second, 2).substring(0,1)) {
+		shifterIntervals[3] = window.setInterval(function() { shiftChar(stringLength(oldSecond, 2).substring(0,1), stringLength(second, 2).substring(0,1), 3); }, shiftDelay);
 	}
-	if (nowTime == 140) {
-		playersReady[6] = true;
-		var d = new Date();
-		var t = d.getTime();
-		var perc = (t-startingTime)/1000.0/startTime;
-		perc = 0.1 + perc*0.8;
-		finishLine(6, 0, perc);
+	if (stringLength(oldMinute, 2).substring(1,2) != stringLength(minute, 2).substring(1,2)) {
+		shifterIntervals[2] = window.setInterval(function() { shiftChar(stringLength(oldMinute, 2).substring(1,2), stringLength(minute, 2).substring(1,2), 2); }, shiftDelay);
 	}
-	if (nowTime == 135) {
-		playersReady[0] = true;
-		var d = new Date();
-		var t = d.getTime();
-		var perc = (t-startingTime)/1000.0/startTime;
-		perc = 0.1 + perc*0.8;
-		finishLine(0, 0, perc);
+	if (stringLength(oldMinute, 2).substring(0,1) != stringLength(minute, 2).substring(0,1)) {
+		shifterIntervals[1] = window.setInterval(function() { shiftChar(stringLength(oldMinute, 2).substring(0,1), stringLength(minute, 2).substring(0,1), 1); }, shiftDelay);
 	}
 }
 
@@ -199,4 +216,115 @@ function linesTimer() {
 
 function playerReady(playerIndex) {
 	playersReady[playerIndex] = true;
+	var d = new Date();
+	var t = d.getTime();
+	var perc = (t-startingTime)/1000.0/startTime;
+	perc = 0.1 + perc*0.8;
+	finishLine(playerIndex, 0, perc);
+	var numberReady = 0;
+	for (var i = 0; i < numberPlayers; i++) {
+		if (playersReady[i]) {
+			numberReady ++;
+		}
+	}
+	shifterIntervals[0] = window.setInterval(function() { shiftChar(numberPlayers-numberReady+1, numberPlayers-numberReady, 0); }, shiftDelay);
+	if (numberReady == numberPlayers) {
+		window.open("showDrawing.html?port=" + serverPort + "&drawing=1", "_self");
+	}
 }
+
+var shiftFps = 40;
+var shiftDur = 0.5 ;		//Seconds
+
+var shiftDelay = 1000/shiftFps;
+var maxShiftTimer = shiftDur*shiftFps;
+
+var shiftTimers = [0, 0, 0, 0, 0];		//readyPlayers, min0, min1, sec0, sec1
+var shifterIntervals = [undefined, undefined, undefined, undefined, undefined];
+
+function shiftChar(prev, next, shiftIndex) {
+	if (shiftTimers[shiftIndex] >= maxShiftTimer+1) {
+		window.clearInterval(shifterIntervals[shiftIndex]);
+		shiftTimers[shiftIndex] = 0;
+		return;
+	}
+	var shiftCtx;
+	if (shiftIndex == 0) {
+		shiftCtx = document.getElementById("leftTitle").getContext('2d');
+	}
+	else {
+		shiftCtx = document.getElementById("rightTitle").getContext('2d');
+	}
+	shiftCtx.fillStyle = "#2C3E50";
+	shiftCtx.textBaseline = "middle"
+	shiftCtx.textAlign = "center";
+	shiftCtx.font = "50px Arial";
+	var xpos;
+	switch (shiftIndex) {
+		case 0:
+			xpos = 75;
+			break;
+		case 1:
+			xpos = 20;
+			break;
+		case 2:
+			xpos = 50;
+			break;
+		case 3:
+			xpos = 100;
+			break;
+		case 4:
+			xpos = 130;
+			break;
+	}
+	var ypos = 55 + shiftTimers[shiftIndex]*1.0/maxShiftTimer*100;
+	shiftCtx.clearRect(xpos-15, 0, 30, 100);
+	shiftCtx.fillText(prev, xpos, ypos);
+	shiftCtx.fillText(next, xpos, ypos-100);
+	shiftTimers[shiftIndex] = shiftTimers[shiftIndex] + 1;
+}
+
+function drawInit() {
+	var ctx1 = document.getElementById("leftTitle").getContext('2d');
+	ctx1.fillStyle = "#2C3E50";
+	ctx1.textBaseline = "middle"
+	ctx1.textAlign = "center";
+	ctx1.font = "50px Arial";
+	ctx1.fillText(numberPlayers, 75, 55);
+	
+	var ctx2 = document.getElementById("rightTitle").getContext('2d');
+	var seconds = nowTime%60;
+	var minutes = (nowTime-seconds)/60;
+	ctx2.fillStyle = "#2C3E50";
+	ctx2.textBaseline = "middle"
+	ctx2.textAlign = "center";
+	ctx2.font = "50px Arial";
+	ctx2.fillText(stringLength(minutes, 2).substring(0,1), 20, 55);
+	ctx2.fillText(stringLength(minutes, 2).substring(1,2), 50, 55);
+	ctx2.fillText(":", 75, 50);
+	ctx2.fillText(stringLength(seconds, 2).substring(0,1), 100, 55);
+	ctx2.fillText(stringLength(seconds, 2).substring(1,2), 130, 55);
+}
+
+function stringLength(number, length) {
+	var result = "" + number;
+	var l = result.length;
+	if (l < length) {
+		for (var i = 0; i < length-l; i++) {
+			result = "0" + result;
+		}
+	}
+	else {
+		if (l > length) {
+			result = result.substring(l-length, l);
+		}
+	}
+	return result;
+}
+
+
+
+
+
+
+
---
+++ WeatherStationSite/WebContent/Scripts/nl/dixit/showDrawing.js
@@ -0,0 +1,183 @@
+window.onerror = function(msg, url, line) {
+	var urlstr = "<a href = " + url + ">" + url + "</a>";
+	var str = "";
+	str = str + "ERROR".fontcolor("red") + "<br>";
+	str = str + msg + "<br>";
+	str = str + "in : ".fontcolor("red") + urlstr + " line : ".fontcolor("red") + line;
+	document.write(str);
+}
+
+window.onresize = function(e) {
+	document.getElementById("mainBlock").style.height = window.innerHeight-70 + "px";
+}
+
+function init() {
+	document.getElementById("mainBlock").style.height = window.innerHeight-70 + "px";
+	getParams();
+}
+
+var serverAddress = 'ws://127.0.0.1:';
+var serverPort;
+var drawingNumber;
+var connection;
+
+function getParams() {
+	var queryString = decodeURIComponent(window.location.search);
+	queryString = queryString.substring(1);
+	var queries = queryString.split("&");
+	serverPort = parseInt(queries[0].split("=")[1]);
+	drawingNumber = parseInt(queries[1].split("=")[1]);
+	
+	document.getElementById("nameHolder").innerHTML = "Tekening " + drawingNumber;
+	
+	connection = new WebSocket(serverAddress + serverPort);
+	
+	connection.onopen = function() {
+		connection.send('getDrawing#' + drawingNumber);
+	};
+	
+	connection.onerror = function (error) {
+	    document.write('WebSocket Error ' + error + "<br>");
+	};
+	
+	connection.onmessage = function (e) {
+		parts = e.data.split("#");
+	    switch (parts[0]) {
+	    	case "drawing":
+	    		startTime = parseInt(parts[1]);
+	    		nowTime = startTime;
+	    		var drawing = unPackDrawing(parts[2]);
+	    		window.setInterval(timer, 1000);
+	    		redraw(drawing);
+	    		break;
+	    }
+	};
+}
+
+function unPackDrawing(drawingStr) {
+	var segments = drawingStr.split("%");
+	var drawingXY = [];
+	for (var i = 0; i < segments.length; i++) {
+		var seg = segments[i].split("&");
+		var segmentColor = "#" + seg[0];
+		var segmentSize = parseInt(seg[1]);
+		var segmentLength = parseInt(seg[2]);
+		var segx = [];
+		var segy = [];
+		for (var j = 0; j < segmentLength; j++) {
+			segx.push(parseInt(seg[3+j*2]));
+			segy.push(parseInt(seg[4+j*2]));
+		}
+		var segment = {color: segmentColor, size: segmentSize, x: segx, y: segy};
+		drawingXY.push(segment);
+	}
+	return drawingXY;
+}
+
+var redrawPps = 40;		//Points/second
+var segmentDelay = 200;	//Delay in milliseconds between two segments
+var maxRedrawTime = 3;	//Max redraw time in seconds
+
+var drawingIndex = 0;
+//var redrawCans = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+//var redrawCtxs = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+var nbDrawings = 0;
+var redrawSegment = 0;
+var redrawPoint = 1;
+var redrawInterval;
+var redrawDelay = 1000/redrawPps;
+var segmentWaitCntMax = segmentDelay/redrawDelay;
+var segmentWaitCnt = 0;
+
+function redraw(drawingXY) {
+	nbDrawings = 0;
+	for (var i = 0; i < drawingXY.length; i++) {
+		if (drawingXY[i].length == 1) {
+			nbDrawings = nbDrawings + 1;
+		}
+		else {
+			nbDrawings = nbDrawings + drawingXY[i].x.length - 1;
+		}
+	}
+	if (nbDrawings*1.0/redrawPps + (drawingXY.length-1)*segmentDelay*1.0/1000 > maxRedrawTime) {
+		redrawPps = Math.ceil(nbDrawings*1.0/maxRedrawTime);
+		redrawDelay = 1000/redrawPps;
+		segmentWaitCntMax = Math.round(segmentDelay/redrawDelay);
+	}
+	//redrawInterval = window.setInterval(redrawPoints, redrawDelay);
+	redrawInterval = window.setInterval(function() { redrawPoints(drawingXY); }, redrawDelay);
+}
+
+function redrawPoints(drawingXY) {
+	if (drawingIndex < nbDrawings) {
+		if (segmentWaitCnt != 0) {
+			segmentWaitCnt --;
+			return;
+		}
+		var segment = drawingXY[redrawSegment];
+		var segmentColor = segment.color;
+		
+		var segmentSize = segment.size;
+		var redrawCtx = document.getElementById("sketchpad").getContext('2d');
+		redrawCtx.fillStyle = segmentColor;
+		redrawCtx.beginPath();
+		
+		if (segment.x.length == 1) {
+			redrawCtx.arc(segment.x[0], segment.y[0], segmentSize, 0, Math.PI*2, true);
+			redrawCtx.closePath();
+			redrawCtx.fill();
+		}
+		else {
+			redrawCtx.strokeStyle = segmentColor;
+			redrawCtx.lineWidth = 2*segmentSize;
+			redrawCtx.lineCap = "round";
+			redrawCtx.moveTo(segment.x[redrawPoint-1],segment.y[redrawPoint-1]);
+			redrawCtx.lineTo(segment.x[redrawPoint],segment.y[redrawPoint]);
+			redrawCtx.stroke();
+		}
+		if (redrawPoint >= segment.x.length-1) {
+			redrawSegment ++;
+			redrawPoint = 1;
+			segmentWaitCnt = segmentWaitCntMax-1;
+		}
+		else {
+			redrawPoint ++;
+		}
+		drawingIndex ++;
+	}
+	else {
+		window.clearInterval(redrawInterval);
+	}
+}
+
+var startTime;	//Seconds
+var nowTime;
+
+function timer() {
+	var sec = nowTime%60;
+	var min = Math.round((nowTime-sec)/60);
+	var timeHolder = document.getElementById("timeHolder");
+	timeHolder.innerHTML = stringLength(min,2) + ":" + stringLength(sec, 2);
+	var loadCan = document.getElementById("loadCan");
+	var loadCtx = loadCan.getContext('2d');
+	var width = (startTime-nowTime)*1.0/startTime*loadCan.width;
+	loadCtx.fillStyle = "#D98880";
+	loadCtx.fillRect(0, 0, width, loadCan.height);
+	nowTime = nowTime - 1;
+}
+
+function stringLength(number, length) {
+	var result = "" + number;
+	var l = result.length;
+	if (l < length) {
+		for (var i = 0; i < length-l; i++) {
+			result = "0" + result;
+		}
+	}
+	else {
+		if (l > length) {
+			result = result.substring(l-length, l);
+		}
+	}
+	return result;
+}
